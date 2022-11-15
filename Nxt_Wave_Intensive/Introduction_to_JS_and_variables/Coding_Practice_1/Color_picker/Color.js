10e89a15fff0817c059afa72876c6db3b9e57da3
Hari <111624220+Hari-MVS@users.noreply.github.com>
2022-11-15 14:20:59 +0530

    Create Color.js

---
+++ Nxt_Wave_Intensive/Introduction_to_JS_and_variables/Coding_Practice_1/Color_picker/Color.js
@@ -0,0 +1,22 @@
+let container = document.getElementById('colorPickerContainer');
+let span = document.getElementById('selectedColorHexCode');
+
+function bt1() {
+    container.style.backgroundColor = '#e0e0e0';
+    span.textContent = "#e0e0e0";
+}
+
+function bt2() {
+    container.style.backgroundColor = '#6fcf97';
+    span.textContent = "#6fcf97";
+}
+
+function bt3() {
+    container.style.backgroundColor = '#56ccf2';
+    span.textContent = "#56ccf2";
+}
+
+function bt4() {
+    container.style.backgroundColor = '#bb6bd9';
+    span.textContent = "#bb6bd9";
+}
