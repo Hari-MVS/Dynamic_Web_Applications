f9b7477465ff19f662a70faeddec6dcd990fc977
Hari <111624220+Hari-MVS@users.noreply.github.com>
2022-11-17 11:17:06 +0530

    Update tab.js

--- Nxt_Wave_Intensive/Arrays_and_Objects/Coding_practice_5/Tabs/tab.js
+++ Nxt_Wave_Intensive/Arrays_and_Objects/Coding_practice_5/Tabs/tab.js
@@ -7,15 +7,17 @@ let tt = document.getElementById('timeToVisitTab');
 let att = document.getElementById('attractionsTab');
 
 ab.onclick = function() {
+    at.classList.remove('d-none')
     tt.classList.add('d-none');
     att.classList.add('d-none');
 }
 tb.onclick = function() {
+    tt.classList.remove('d-none')
     at.classList.add('d-none');
     att.classList.add('d-none');
 }
 atb.onclick = function() {
-
+    att.classList.remove('d-none')
     tt.classList.add('d-none');
     at.classList.add('d-none');
 }
