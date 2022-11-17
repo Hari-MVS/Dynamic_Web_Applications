846393c647a84dc05a923218764f485a050b2250
Hari <111624220+Hari-MVS@users.noreply.github.com>
2022-11-17 11:32:36 +0530

    Update tab.js

--- Nxt_Wave_Intensive/Arrays_and_Objects/Coding_practice_5/Tabs/tab.js
+++ Nxt_Wave_Intensive/Arrays_and_Objects/Coding_practice_5/Tabs/tab.js
@@ -7,17 +7,30 @@ let tt = document.getElementById('timeToVisitTab');
 let att = document.getElementById('attractionsTab');
 
 ab.onclick = function() {
-    at.classList.remove('d-none')
+    at.classList.remove('d-none');
     tt.classList.add('d-none');
     att.classList.add('d-none');
+    
+    ab.classList.add('selected-button');
+    tb.classList.remove('selected-button');
+    atb.classList.remove('selected-button');
+    
 }
 tb.onclick = function() {
-    tt.classList.remove('d-none')
+    tt.classList.remove('d-none');
     at.classList.add('d-none');
     att.classList.add('d-none');
+    
+    ab.classList.remove('selected-button');
+    tb.classList.add('selected-button');
+    atb.classList.remove('selected-button');
 }
 atb.onclick = function() {
-    att.classList.remove('d-none')
+    att.classList.remove('d-none');
     tt.classList.add('d-none');
     at.classList.add('d-none');
+    
+    ab.classList.remove('selected-button');
+    tb.classList.remove('selected-button');
+    atb.classList.add('selected-button');
 }
