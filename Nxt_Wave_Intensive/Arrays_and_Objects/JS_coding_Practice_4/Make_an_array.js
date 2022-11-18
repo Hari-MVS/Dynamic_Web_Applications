9091b98ae7f1b30b05037d0d6a24c7f67340c4ae
Hari <111624220+Hari-MVS@users.noreply.github.com>
2022-11-18 07:48:34 +0530

    Create Make_an_array.js

---
+++ Nxt_Wave_Intensive/Arrays_and_Objects/JS_coding_Practice_4/Make_an_array.js
@@ -0,0 +1,42 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((str) => str.trim());
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+function main() {
+  // num1
+  let num1 = parseInt(readLine());
+  // num2
+  let num2 = parseInt(readLine());
+  // num3
+  let num3 = parseInt(readLine());
+
+  function makeAnArray(a,b,c) {
+      let list= [a,b,c];
+      return list
+    
+  }
+  
+  let createdArray = makeAnArray(num1, num2, num3);
+  console.log(createdArray);
+}
