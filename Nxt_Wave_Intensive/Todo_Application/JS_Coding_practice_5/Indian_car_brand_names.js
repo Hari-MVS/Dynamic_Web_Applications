4176cfff931326ad46e3e4e225f93478d7a4da40
Hari <111624220+Hari-MVS@users.noreply.github.com>
2022-11-18 07:52:46 +0530

    Create Indian_car_brand_names.js

---
+++ Nxt_Wave_Intensive/Todo_Application/JS_Coding_practice_5/Indian_car_brand_names.js
@@ -0,0 +1,36 @@
+"use strict";
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((str) => str.trim());
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+function main() {
+  // arrayOfCarBrands
+  let arrayOfCarBrands = JSON.parse(readLine().replace(/'/g, '"'));
+  let list=[];
+  for (let i of arrayOfCarBrands){
+      if (i['country']==='India'){
+          list.push(i['name'])
+      }
+  }
+  console.log(list);
+}
