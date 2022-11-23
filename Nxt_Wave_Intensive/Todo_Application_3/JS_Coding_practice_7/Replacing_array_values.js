aea10bbcc84f6e5978056cc21609faf3550b03d7
Hari <111624220+Hari-MVS@users.noreply.github.com>
2022-11-23 10:02:59 +0530

    Update Replacing_array_values.js

--- Nxt_Wave_Intensive/Todo_Application_3/JS_Coding_practice_7/Replacing_array_values.js
+++ Nxt_Wave_Intensive/Todo_Application_3/JS_Coding_practice_7/Replacing_array_values.js
@@ -1,1 +1,36 @@
+"use strict";
 
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((str) => str.trim());
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+function main() {
+  let myArray = JSON.parse(readLine().replace(/'/g, '"'));
+  let startIndex = parseInt(readLine());
+  let deleteCount = parseInt(readLine());
+  let firstVal = JSON.parse(readLine().replace(/'/g, '"'));
+  let secondVal = JSON.parse(readLine().replace(/'/g, '"'));
+  
+  myArray.splice(startIndex,deleteCount,firstVal,secondVal)
+  console.log(myArray)
+  
+}
