d1efa18bd44746cdbfba2f52f22f6dcbc1c0ec1a
Hari <111624220+Hari-MVS@users.noreply.github.com>
2022-11-23 09:59:29 +0530

    Update Remove_first_value.js

--- Nxt_Wave_Intensive/Todo_Application_3/JS_Coding_practice_7/Remove_first_value.js
+++ Nxt_Wave_Intensive/Todo_Application_3/JS_Coding_practice_7/Remove_first_value.js
@@ -1,1 +1,32 @@
+"use strict";
 
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", (_) => {
+  inputString = inputString
+    .trim()
+    .split("\n")
+    .map((str) => str.trim());
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+function main() {
+  let myArray = JSON.parse(readLine().replace(/'/g, '"'));
+  
+  console.log(myArray.shift())
+  console.log(myArray)
+  
+}
