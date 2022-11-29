8999944ef2c2cd1c85820c141a30fd9e87eea926
Hari <111624220+Hari-MVS@users.noreply.github.com>
2022-11-29 11:58:22 +0530

    Create put.js

---
+++ Nxt_Wave_Intensive/Fetch_&_Callback_2/Coding_practice_16/http_put/put.js
@@ -0,0 +1,31 @@
+let txt = document.getElementById('requestBody');
+let btn = document.getElementById('sendPutRequestBtn');
+let rqst = document.getElementById('requestStatus');
+let para = document.getElementById('httpResponse');
+let load = document.getElementById('loading');
+let inp = document.getElementById('userInput');
+
+let url = "https://gorest.co.in/public-api/users" + inp.value;
+
+function pos() {
+    load.classList.remove('d-none');
+    let options = {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: "Bearer c3d87c0eec8cdb5aee4bd44b0f5711860f2c52d64acd4ec7e3b4705381d41672"
+        },
+        body: txt.value
+    };
+    fetch(url, options)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            load.classList.add('d-none');
+            rqst.textContent = data.code;
+            para.textContent = JSON.stringify(data);
+        });
+}
+btn.addEventListener('click', pos);
